7654512823405f1b99d75b364880403612820317
Fred120-code <fredayemtsa@gmail.com>
2026-03-31 18:27:54 +0100

    feat:add type typescript

---
+++ frontend/src/features/social/index.ts
@@ -0,0 +1,13 @@
+export type {
+  Platform,
+  SocialAccountStatus,
+  SocialAccount,
+  GetSocialAccountsResponse,
+  ConnectSocialAccountResponse,
+  DisconnectSocialAccountResponse,
+  ConnectSocialAccountRequest,
+  DisconnectSocialAccountRequest,
+  PlatformConfig,
+} from './types/social.types'
+
+export { PLATFORM_CONFIG } from './types/social.types'
