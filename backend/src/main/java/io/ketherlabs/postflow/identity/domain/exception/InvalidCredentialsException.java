f149cf3f6ebb273b0ec7b92ad0ae0824fe5cd7f9
Mgreat01 <ephraimmonga5@gmail.com>
2026-04-28 16:52:23 +0200

    feat(exception): Ajout de InvalidCredentialsException pour les erreurs d’authentification

---
+++ backend/src/main/java/io/ketherlabs/postflow/identity/domain/exception/InvalidCredentialsException.java
@@ -0,0 +1,8 @@
+package io.ketherlabs.postflow.identity.domain.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+
+    public InvalidCredentialsException() {
+        super("Invalid email or password");
+    }
+}
